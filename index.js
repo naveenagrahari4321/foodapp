27ffff21b651c83a6cae67c2da92b3efa662569e
naveenagrahari4321 <40599954+naveenagrahari4321@users.noreply.github.com>
2020-03-19 15:24:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ var express = require("express");
 var app = express();
 var home = require("./routes/home");
 var home = require("./routes/restaurant");
+const route = require("./routes/userroute");
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-
+app.use(express.static('public'));
 app.use('/',home);
 app.use('/api/restaurant',home);
-
+app.use(route) //for customers route 
 var port = process.env.HTTP_PORT || 5000;
 app.listen(port,()=>{
     console.log(`Listening on port ${port}`)
